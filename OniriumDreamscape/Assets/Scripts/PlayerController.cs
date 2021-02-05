db04c9b483e021efe00c2a9a68688e45731d62ba
RoberInside <roberto.alcazar@campusfp.es>
2021-02-05 14:39:26 +0100

    Movimiento comenzado personaje
    falta que s emueva de iza a dcha.

--- OniriumDreamscape/Assets/Scripts/PlayerController.cs
+++ OniriumDreamscape/Assets/Scripts/PlayerController.cs
@@ -4,51 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject player;
-    public float horiMove;
-    public float vertMove;
-    public float speed;
-    public float jump;
-    public bool isGrounded;
+    
+    [SerializeField, Range(0, 0.5f)]
+    private float distanceToGround = 0.1f;
+
     public Rigidbody playerRB;
+    public LayerMask groundLayer;
+
+    private float _horiMove, _vertMove, _jumpAxis;    
+    private float _speed;
+    private float _jumpForce;
+    private CapsuleCollider _collider;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+        _speed = 5;
+        _jumpForce = 10;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        horiMove = Input.GetAxis("Horizontal");
-        vertMove = Input.GetAxis("Vertical");
+        _horiMove = Input.GetAxis("Horizontal") * 5;
+        _vertMove = Input.GetAxis("Vertical");
+        _jumpAxis = Input.GetAxis("Jump");
 
-        transform.Translate(new Vector3(horiMove, 0, vertMove) * speed * Time.deltaTime);
+        Quaternion angleRot = Quaternion.Euler(Vector3.up * _horiMove * Time.fixedDeltaTime);
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        playerRB.MovePosition(transform.position + transform.forward * _vertMove * Time.fixedDeltaTime);
+        playerRB.MoveRotation(angleRot * playerRB.rotation);
+
+        if (Input.GetAxis("Jump") > 0.5 && IsGrounded())
         {
-            jump = 40;
-            playerRB.AddForce(new Vector3(0, jump * speed, 0));
-            
-            //transform.Translate(new Vector3(0, jump, 0) * Time.deltaTime);
+            Debug.Log(Input.GetAxis("Jump"));
+            playerRB.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
         
+        
     }
-  
-    private void OnCollisionEnter(Collision other)
+    bool IsGrounded()
     {
-        if (other.gameObject.CompareTag("Platform"))
-        {
-            isGrounded = true;
-        }
+        Vector3 capsuleBottom = new Vector3(_collider.bounds.center.x, _collider.bounds.min.y, _collider.bounds.center.z);
+        bool isGrounded = Physics.CheckCapsule(_collider.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);        
+        return isGrounded;
     }
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.CompareTag("Platform"))
-        {
-            isGrounded = false;
-            jump = 0;
-        }
-    }
+
+
 
 }
