73564799a006b26f57057ba1fcd664bd9fcdbe30
RoberInside <roberto.alcazar@campusfp.es>
2021-02-19 19:03:32 +0100

    Navmesh follow
    el enemigo persigue al jugador

--- OniriumDreamscape/Assets/Scripts/EnemyBehaviour.cs
+++ OniriumDreamscape/Assets/Scripts/EnemyBehaviour.cs
@@ -5,20 +5,24 @@ using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    public float maxDelay = 0.5f;
     public Transform playerTF;
     public Transform patrolTF;
+    //public Collider zonaggro;
 
-    public List<Transform> _waypointsDest;
+    public List<Transform> waypoints;
 
     [SerializeField]
     public NavMeshAgent _agent;
+    private float currentDelay = 0;
     private int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerTF = GameObject.Find("Player").GetComponent<Transform>();
-        _agent = GetComponent<NavMeshAgent>();        
+        _agent = GetComponent<NavMeshAgent>();
+        playerTF = GameObject.FindGameObjectWithTag("Player").transform;
+        //zonaggro = GameObject.Find("ZonaAggro").GetComponent<Collider>();
         InitializeWp();
         MoveNextWp();
     }
@@ -26,18 +30,47 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveNextWp();
+        if (_agent.remainingDistance < 0.5f && !_agent.pathPending)
+        {
+            currentDelay += Time.deltaTime;
+            if (currentDelay > maxDelay)
+            {
+                currentDelay = 0;
+                MoveNextWp();                
+            }
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        //other = zonaggro;
+        if (other.CompareTag("Player"))
+        {
+            _agent.SetDestination(playerTF.position);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            MoveNextWp();
+            Debug.Log("Jugador perdido");
+        }
     }
     void InitializeWp()
     {
-        foreach (Transform transform in _waypointsDest)
+        foreach (Transform wp in patrolTF)
         {
-            _waypointsDest.Add(transform);
+            waypoints.Add(wp);
         }
     }
     void MoveNextWp()
     {
-        _agent.SetDestination(_waypointsDest[index].position);
-        index = Random.Range(0, _waypointsDest.Count);
+        if (waypoints.Count == 0)
+        {
+            return;
+        }
+        _agent.SetDestination(waypoints[index].position);
+        index = Random.Range(0, waypoints.Count);
     }
 }
