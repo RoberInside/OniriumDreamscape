363bb1fb4380a307376a6dc0e8f3554faaaf94aa
RoberInside <roberto.alcazar@campusfp.es>
2021-02-20 14:35:27 +0100

    Navmesh enemigo acabado
    falta colocar las zonas transitables en el main

--- OniriumDreamscape/Assets/Scripts/EnemyBehaviour.cs
+++ OniriumDreamscape/Assets/Scripts/EnemyBehaviour.cs
@@ -8,21 +8,23 @@ public class EnemyBehaviour : MonoBehaviour
     public float maxDelay = 0.5f;
     public Transform playerTF;
     public Transform patrolTF;
-    //public Collider zonaggro;
+    public NavMeshAgent agent;
+    public float defaultSpeed;
+    
 
     public List<Transform> waypoints;
 
     [SerializeField]
-    public NavMeshAgent _agent;
     private float currentDelay = 0;
     private int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        _agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         playerTF = GameObject.FindGameObjectWithTag("Player").transform;
-        //zonaggro = GameObject.Find("ZonaAggro").GetComponent<Collider>();
+        defaultSpeed = 3.5f;
+
         InitializeWp();
         MoveNextWp();
     }
@@ -30,7 +32,7 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_agent.remainingDistance < 0.5f && !_agent.pathPending)
+        if (agent.remainingDistance < 0.5f && !agent.pathPending)
         {
             currentDelay += Time.deltaTime;
             if (currentDelay > maxDelay)
@@ -38,23 +40,6 @@ public class EnemyBehaviour : MonoBehaviour
                 currentDelay = 0;
                 MoveNextWp();                
             }
-        }
-
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        //other = zonaggro;
-        if (other.CompareTag("Player"))
-        {
-            _agent.SetDestination(playerTF.position);
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.name == "Player")
-        {
-            MoveNextWp();
-            Debug.Log("Jugador perdido");
         }
     }
     void InitializeWp()
@@ -64,13 +49,13 @@ public class EnemyBehaviour : MonoBehaviour
             waypoints.Add(wp);
         }
     }
-    void MoveNextWp()
+    public void MoveNextWp()
     {
         if (waypoints.Count == 0)
         {
             return;
         }
-        _agent.SetDestination(waypoints[index].position);
+        agent.SetDestination(waypoints[index].position);
         index = Random.Range(0, waypoints.Count);
     }
 }
